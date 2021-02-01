c420289785dc410eab2fd7e642543807ff5d613b
Ted Brown <ted@oreganik.com>
2021-01-31 16:20:44 -0800

    Updated giver/receiver triggers with chevrons

---
+++ Assets/Prototype/Code/Chevron.cs
@@ -0,0 +1,31 @@
+﻿// GLOBAL GAME JAM 2021
+// Shaquan Ladson & Ted Brown
+
+using Jambox;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Chevron : MonoBehaviour 
+	{
+		static float BounceHeight = 1;
+		static float SinScaler = 3;
+		private Vector3 _basePosition;
+
+		protected void Awake ()
+		{
+			_basePosition = transform.localPosition;
+		}
+
+		protected void Update ()
+		{
+			float t = (1 + Mathf.Sin(Time.timeSinceLevelLoad* 3)) / 2;
+			transform.localPosition = _basePosition + Vector3.up * t * BounceHeight;
+		}
+	}
+}
